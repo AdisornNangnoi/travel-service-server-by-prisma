588f6aad303b248e903a9b4dc571ec1f56c7ddf1
Adisorn <s6552410001@sau.ac.th>
2025-04-02 16:05:21 +0700

    ฟกฟ

--- server.js
+++ server.js
@@ -9,8 +9,15 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors()); //เรียกข้ามโดเมน
-app.use(express.json()); //เรียกใช้ json
+const corsOptions = {
+  origin: 'https://travel-dti-app-by-axios-mu.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // ถ้า backend ของคุณต้องการจัดการ credentials (cookies, authorization headers)
+  optionsSuccessStatus: 204, // สำหรับบางเบราว์เซอร์
+};
+
+app.use(cors(corsOptions)); // เรียกข้ามโดเมนโดยกำหนดค่า
+app.use(express.json()); // เรียกใช้ json
 app.use('/traveller', travellerRoutes);
 app.use('/travel', travelRoutes);
 //กำหนดการเข้าถึงรูปภาพ
@@ -18,11 +25,11 @@ app.use('/images/travel', express.static('images/travel'));
 app.use('/images/traveller', express.static('images/traveller'));
 
 app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello from backend server!' 
-    });
+  res.json({
+    message: 'Hello from backend server!'
+  });
 });
-   
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}...`);
+  console.log(`Server is running on port ${PORT}...`);
 });
